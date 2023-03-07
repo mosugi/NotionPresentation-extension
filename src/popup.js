e95d639a8fd16b228f9105ed0350a130ee94ac92
mosugi <mosuging@gmail.com>
2023-03-07 13:28:23 +0900

    Content

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,24 @@
 import "./popup.css";
+import { startPresentation } from "./content";
 
 document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <button id="start">Start Presentation</button>
 `;
+
+const executeScript = async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  if (tab.url.includes("notion.site") || tab.url.includes("notion.so")) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: startPresentation,
+    });
+  } else {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: startPresentation,
+      // function: () => alert("Please open *.notion.site or *.notion.so and click again!"),
+    });
+  }
+};
+
+document.querySelector("#start").addEventListener("click", executeScript);
