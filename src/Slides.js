4b13e5742865f688014f99690e56906b9d19f0ce
mosugi <mosuging@gmail.com>
2023-03-07 14:41:50 +0900

    Refactor

---
+++ src/Slides.js
@@ -0,0 +1,39 @@
+export const slides = (() => {
+    let chunkBlocks = [];
+
+    // first page
+    chunkBlocks.push(document.querySelector(".pseudoSelection:first-of-type"));
+    chunkBlocks.push(
+        document.querySelector(".pseudoSelection:first-of-type + div")
+    );
+
+    const notionPageContent = Array.from(
+        document.querySelectorAll(".notion-page-content .notion-selectable")
+    );
+
+    let pages = [];
+
+    notionPageContent.forEach((it, i, arr) => {
+        if (!it.className.includes("notion-header-block")) {
+            chunkBlocks.push(it);
+        } else {
+            pages.push(chunkBlocks);
+            chunkBlocks = [];
+            chunkBlocks.push(it);
+        }
+
+        if (i === arr.length - 1) {
+            pages.push(chunkBlocks);
+        }
+    });
+
+    let i = 0;
+    return {
+        all: () => pages,
+        first: () => pages[0],
+        last: () => pages[pages.length - 1],
+        current: () => pages[i],
+        next: () => (i < pages.length - 1 ? pages[++i] : pages[i]),
+        previous: () => (i > 0 ? pages[--i] : pages[i]),
+    };
+})();
---
+++ src/Style.js
@@ -0,0 +1,37 @@
+const slideIn =
+    "<style>@keyframes transition { from { margin-left: 100%; width: 300%;}to {margin-left: 0%;width: 100%;}}</style>";
+const fadeIn =
+    "<style>@keyframes transition { from { opacity:0 }to {opacity:1;}}</style>";
+const fadeOut =
+    "<style>@keyframes fadeOut { from { opacity:1 }to {opacity:0;}}</style>";
+const animation =
+    "<style>.notion-selectable {animation-duration: 0.5s;animation-name: transition;}</style>";
+export const showPageBlocks = (blocks) => {
+    blocks.forEach((it) => (it.style.display = "block"));
+};
+export const hidePageBlocks = (blocks) => {
+    blocks.forEach((it) => (it.style.display = "none"));
+};
+export const styleFirstPage = () => {
+    document.querySelector(".pseudoSelection > div").style.height = "80vh"; // original 30vh
+    document.querySelector(".pseudoSelection > div > div > div").style.height =
+        "80vh"; // original 30vh
+    document.querySelector(
+        ".pseudoSelection > div > div > div > div > img"
+    ).style.height = "80vh"; // original 30vh
+    document.querySelector(
+        ".pseudoSelection:first-of-type + div > div"
+    ).style.width = "100%"; // original 900px
+    document.querySelector(
+        ".pseudoSelection:first-of-type + div > div"
+    ).style.textAlign = "center"; // original ''
+};
+export const hiddenControls = () => {
+    document.querySelector(".notion-topbar").style.display = "none";
+    document.querySelector(".notion-page-controls").style.display = "none";
+};
+export const addAnimation = () => {
+    document.querySelector("head").insertAdjacentHTML("beforeend", fadeIn);
+    document.querySelector("head").insertAdjacentHTML("beforeend", fadeOut);
+    document.querySelector("head").insertAdjacentHTML("beforeend", animation);
+};
